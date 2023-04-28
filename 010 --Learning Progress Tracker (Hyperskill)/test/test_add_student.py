1832b33c6b241ffd5c30077a2ac32aa6953760fb
TomasDolejsek <tdolejsek@centrum.cz>
2023-04-28 20:08:37 +0200

    Update test_add_student.py

--- 010 --Learning Progress Tracker (Hyperskill)/test/test_add_student.py
+++ 010 --Learning Progress Tracker (Hyperskill)/test/test_add_student.py
@@ -10,22 +10,22 @@ class TestCase(unittest.TestCase):
         "Tom Dol email@cz.",
         "Tom Dol @email.cz",
         "Tom D email@cz.com",
-        "Tom D. email@cz.com",
-        "Tom Dol1 email@cz.com",
-        "Tom Dol-Bol-Vol- email@cz.com",
-        "Tom Dol' email@cz.com",
-        "Tom D--sek email@cz.com",
-        "Tom D'-lejs email@cz.com",
-        "Tom- DolBolVol email@cz.com",
-        "T DolBolVol email@cz.com",
-        "Tom' DolBolVol email@cz.com",
-        "'Tom DolBolVol email@cz.com"
+        "Tom D. email1@cz.com",
+        "Tom Dol1 email2@cz.com",
+        "Tom Dol-Bol-Vol- email3@cz.com",
+        "Tom Dol' email4@cz.com",
+        "Tom D--sek email5@cz.com",
+        "Tom D'-lejs email6@cz.com",
+        "Tom- DolBolVol email7@cz.com",
+        "T DolBolVol email8@cz.com",
+        "Tom' DolBolVol email9@cz.com",
+        "'Tom DolBolVol email10@cz.com"
         ]
 
     correct_input = [
-        "Tom DolBolVol email@cz.com",
-        "Tom D'Ejsek email@cz.com",
-        "Tom Dol-Bol-Vol email@cz.com"
+        "Tom DolBolVol email11@cz.com",
+        "Tom D'Ejsek email12@cz.com",
+        "Tom Dol-Bol-Vol email13@cz.com"
         ]
 
     def test_add_student(self):
