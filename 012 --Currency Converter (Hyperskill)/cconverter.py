6ea3ebd66838552ac300b157518101efff886914
TomasDolejsek <tdolejsek@centrum.cz>
2023-05-05 07:00:43 +0200

    Update cconverter.py

--- 012 --Currency Converter (Hyperskill)/cconverter.py
+++ 012 --Currency Converter (Hyperskill)/cconverter.py
@@ -1,9 +1,25 @@
-currencies = {'RUB': 2.98, 'ARS': 0.82, 'HNL': 0.17, 'AUD': 1.9622, 'MAD': 0.208}
-try:
-    howmany = float(input())
-    if howmany <= 0:
-        raise ValueError
-    for cur, val in currencies.items():
-        print(f"I will get {howmany * val:.2f} {cur} from the sale of {howmany:.1f} conicoins.")
-except ValueError:
-    print("You should enter a number > 0")
+import json
+import requests
+
+
+class Converter:
+    def __init__(self):
+        self.urlpage = 'http://www.floatrates.com/daily/'
+        self.currencies = ('ARS', 'AUD', 'CZK', 'HNL', 'MAD', 'RUB')
+        self.start()
+
+    def start(self):
+        user = input().upper()
+        if user not in self.currencies:
+            print("Unsupported currency.")
+            exit()
+        url = self.urlpage + user.lower() + '.json'
+        page = requests.get(url)
+        data = json.loads(page.text)
+        for cur, info in data.items():
+            if cur == 'usd' or key == 'eur':
+                print(info)
+
+
+if __name__ == '__main__':
+    Converter()
