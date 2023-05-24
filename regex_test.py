3e7d22dbd6d7dda6650f625f7c253e79e49c8227
TomasDolejsek <tdolejsek@centrum.cz>
2023-05-24 17:43:22 +0200

    Tomas Dolejsek

--- regex_test.py
+++ regex_test.py
@@ -3,8 +3,8 @@ import re
 #"^[+-]?([0-9]+|[a-zA-Z]+)([ ]*([+-]+|[*/^]?)([ ]*[+-]*)?([0-9]+|[a-zA-Z]+))*$"
 #"^[+-]?([0-9]+|[a-zA-Z]+)([ ]*([+-]+|[*/^]?)([ ]*[+-^]*)?([0-9]+|[a-zA-Z]+))*$"
 
-pattern = '^Good [a-z]*$'
+pattern = '^s'
 
-name = "Good evening!"
+name = "username"
 
 print(re.match(pattern, name) is not None)
