da31fd6e2fb1d26fde99a5c95722357dbddedd4c
hyper <tdolejsek@centrum.cz>
2023-05-29 07:50:23 +0200

    Create game.py

---
+++ 03 Python Core/17 Knight's Tour Puzzle/game.py
@@ -0,0 +1,37 @@
+class ChessBoard:
+    def __init__(self, knight_pos):
+        self.knight_pos = knight_pos
+        self.display()
+
+    def display(self):
+        print(f" {'-' * 19}")
+        for i in reversed(range(1, 9)):
+            line = '_ ' * 8
+            if i == self.knight_pos[1]:
+                line = line[:(self.knight_pos[0] * 2) - 2] + 'X' + line[(self.knight_pos[0] * 2) - 1:]
+            print(f"{i}| {line}|")
+        print(f" {'-' * 19}")
+        print("   1 2 3 4 5 6 7 8")
+
+
+class UserInterface:
+    def __init__(self):
+        self.start()
+
+    def start(self):
+        knight_pos = input("Enter the knight's starting position: ").split()
+        try:
+            if len(knight_pos) != 2:
+                raise ValueError
+            knight_pos = list(map(lambda x: int(x), knight_pos))
+            for el in knight_pos:
+                if not (1<= el <= 8):
+                    raise ValueError
+        except ValueError:
+            print("Invalid dimensions!")
+            exit()
+        ChessBoard(knight_pos)
+
+
+if __name__ == '__main__':
+    UserInterface()
