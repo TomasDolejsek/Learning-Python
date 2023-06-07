0db0b331bd51ab3fa2940283d790c264da43793d
TomasDolejsek <tdolejsek@centrum.cz>
2023-06-07 15:18:43 +0200

    Update duskers.py

--- 04 Practicing Algorithms with Python/14 Duskers/duskers.py
+++ 04 Practicing Algorithms with Python/14 Duskers/duskers.py
@@ -1,6 +1,9 @@
 import argparse
 import random
 import time
+import os
+from datetime import datetime
+import json
 
 
 class UserInterface:
@@ -28,10 +31,17 @@ class Game(UserInterface):
 
     def explore(self):
         max_locations = random.randint(1, 9)
-        locations = dict()
-        command = 's'
+        loc_name = random.choice(self.possible_locations)
+        titanium = random.randint(10, 100)
+        locations = {'1': [loc_name, titanium]}
+        print("Searching", end='')
+        self.animate()
         while True:
             nlocations = len(locations)
+            for key, value in locations.items():
+                print(f"[{key}] {value[0]}")
+            print("\n[S] to continue searching")
+            command = self.get_command([*list(locations.keys()), 's', 'back'])
             if command == 's':
                 if nlocations < max_locations:
                     loc = random.choice(self.possible_locations)
@@ -41,10 +51,6 @@ class Game(UserInterface):
                     self.animate()
                 else:
                     print("Nothing more in sight.")
-                for key, value in locations.items():
-                    print(f"[{key}] {value[0]}")
-                print("\n[S] to continue searching")
-                command = self.get_command([*list(locations.keys()), 's', 'back'])
                 continue
             if command == 'back':
                 return 0
@@ -53,7 +59,7 @@ class Game(UserInterface):
 
     def animate(self):
         if self.delay != 0:
-            for second in range(self.delay):
+            for second in range(self.delay + 1):
                 time.sleep(1)
                 print('.', end='')
         print()
@@ -83,42 +89,51 @@ class Hub(UserInterface):
                       "        _)(  )(_          ",
                       "       `---''---`         ")
         self.score = 0
+        self.player = 'player 1'
+        self.nrobots = 3
         self.game = Game(args)
 
-    def play(self, name):
+    def new_game(self):
         menu = ['[Yes]', '[No]', 'Return to Main[Menu]']
-        print(f"\nGreetings, commander {name}!")
+        print(f"\nGreetings, commander {self.player}!")
         while True:
             print('Are you ready to begin?')
             print(*menu, sep=' ')
             command = self.get_command(['yes', 'no', 'menu'])
             if command == 'yes':
-                while True:
-                    self.display()
-                    command = self.get_command(['ex', 'up', 'save', 'm'])
-                    if command == 'ex':
-                        self.score += self.game.explore()
-                        continue
-                    if command == 'm':
-                        answer = self.game_menu()
-                        if answer == 'back':
-                            continue
-                        if answer == 'main':
-                            return
-                        if answer == 'save':
-                            self.save()
-                            continue
-                        else:
-                            print("Thanks for playing, bye!")
-                            exit()
-                    else:
-                        print("Coming SOON! Thanks for playing!")
-                        exit()
+                self.play()
+                return
             if command == 'no':
                 print("\nHow about now.")
                 continue
             if command == 'menu':
                 return
+
+    def play(self):
+        while True:
+            self.display()
+            command = self.get_command(['ex', 'up', 'save', 'm'])
+            if command == 'ex':
+                self.score += self.game.explore()
+                continue
+            if command == 'save':
+                self.save()
+                continue
+            if command == 'm':
+                answer = self.game_menu()
+                if answer == 'back':
+                    continue
+                if answer == 'main':
+                    return
+                if answer == 'save':
+                    self.save()
+                    continue
+                else:
+                    print("Thanks for playing, bye!")
+                    exit()
+            else:
+                print("Coming SOON! Thanks for playing!")
+                exit()
 
     def display(self):
         border = "+===============================================================================+"
@@ -159,10 +174,53 @@ class Hub(UserInterface):
         print("Coming SOON! Thanks for playing!")
         exit()
 
-    @staticmethod
-    def save():
-        print("Coming SOON! Thanks for playing!")
-        exit()
+    def save_load_menu(self):
+        menu = {'1': 'empty', '2': 'empty', '3': 'empty'}
+        for i, data in enumerate(self.get_savegames()):
+            if data:
+                menu[str(i + 1)] = data
+        print("Select save slot:")
+        for key, data in menu.items():
+            if data == 'empty':
+                print(f"[{key}] {data}")
+            else:
+                print(f"[{key}] {data['Player']} Titanium: {data['Titanium']} Robots: {data['Robots']} Last save: {data['Last_save']}")
+        return self.get_command([*menu.keys(), 'back'])
+
+    def save(self):
+        data_to_save = {'Player': self.player, 'Titanium': self.score, 'Robots': self.nrobots}
+        command = self.save_load_menu()
+        if command == 'back':
+            return
+        now = datetime.now()
+        date_time_str = now.strftime("%Y-%m-%d %H:%M")
+        data_to_save['Last_save'] = date_time_str
+        with open('save_file' + command, 'w') as file:
+            json.dump(data_to_save, file)
+
+    def get_savegames(self):
+        data_list = list()
+        scan = os.listdir()
+        for filename in scan:
+            if os.path.isfile(filename) and filename.startswith('save_file'):
+                with open(filename, 'r') as file:
+                    data_list.append(json.load(file))
+        return data_list
+
+    def load(self):
+        border = '                      |==============================|'
+        command = self.save_load_menu()
+        if command == 'back':
+            return
+        with open('save_file' + command, 'r') as file:
+            hub_data = json.load(file)
+        self.player = hub_data['Player']
+        self.score = hub_data['Titanium']
+        self.nrobots = hub_data['Robots']
+        print(border)
+        print('                     |    GAME LOADED SUCCESSFULLY  |')
+        print(border)
+        print(f"Welcome back, commander {self.player}")
 
 
 class MainMenu(UserInterface):
@@ -176,7 +234,7 @@ class MainMenu(UserInterface):
                      "██████╔╝╚██████╔╝██████╔╝██║░╚██╗███████╗██║░░██║██████╔╝",
                      "╚═════╝░░╚═════╝░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═════╝░",
                      "             (Survival ASCII Strategy Game)              ")
-        self.menu = ('[Play]', '[High] Scores', '[Help]', '[Exit]')
+        self.menu = ('[New] Game', '[Load] Game', '[High] Scores', '[Help]', '[Exit]')
         self.args = self.get_arguments()
         self.start()
 
@@ -193,11 +251,16 @@ class MainMenu(UserInterface):
         while True:
             print(*self.LOGO, sep='\n')
             print(*self.menu, sep='\n')
-            command = self.get_command(['play', 'high', 'help', 'exit'])
-            if command == 'play':
+            command = self.get_command(['new', 'load', 'high', 'help', 'exit'])
+            if command == 'new':
                 print("\nEnter your name:")
                 name = input().strip()
-                hub.play(name)
+                hub.player = name
+                hub.new_game()
+                continue
+            if command == 'load':
+                hub.load()
+                hub.play()
                 continue
             if command == 'high':
                 hub.high_score()
