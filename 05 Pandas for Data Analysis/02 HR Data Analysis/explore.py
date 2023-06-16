76793d7c32f025f3f3fcf1ec239916cb548021db
hyper <tdolejsek@centrum.cz>
2023-06-16 08:10:24 +0200

    Update explore.py

--- 05 Pandas for Data Analysis/02 HR Data Analysis/explore.py
+++ 05 Pandas for Data Analysis/02 HR Data Analysis/explore.py
@@ -5,6 +5,10 @@ class DataAnalysator:
     def __init__(self):
         self.start()
 
+    @staticmethod
+    def count_bigger_5(series):
+        return series.where(series > 5).count()
+
     def start(self):
         # stage 1
         a_data = pd.read_xml('../Data/A_office_data.xml')
@@ -27,16 +31,34 @@ class DataAnalysator:
 
         # stage 3
         hard_working = ab_hr_data.sort_values('average_monthly_hours', ascending=False)['Department']
-        print(hard_working.head(10).tolist())
+        # print(hard_working.head(10).tolist())
         projects = sum(ab_hr_data['number_project'].where((ab_hr_data.Department == 'IT')
                                                     & (ab_hr_data.salary == 'low'), 0))
-        print(projects)
+        # print(projects)
         employees = ['A4', 'B7064', 'A3033']
         employees_data = []
         for employee in employees:
             data = ab_hr_data.loc[(ab_hr_data.index == employee), ['last_evaluation', 'satisfaction_level']]
             employees_data.append(data.values.tolist()[0])
-        print(employees_data)
+        # print(employees_data)
+
+        # stage 4
+        boss_table = ab_hr_data.groupby(['left']).agg({'number_project': ['median', self.count_bigger_5],
+                                                       'time_spend_company': ['mean', 'median'],
+                                                       'Work_accident': 'mean',
+                                                       'last_evaluation': ['mean', 'std']})
+        # print(boss_table.round(2).to_dict())
+
+        # stage 5
+        piv_tab1 = ab_hr_data.pivot_table(index='Department', columns=['left', 'salary'], values='average_monthly_hours',
+                                          aggfunc='median')
+        disp_tab1 = piv_tab1.loc[(piv_tab1[(0, 'high')].values < piv_tab1[(0, 'medium')].values) |
+                                 (piv_tab1[(1, 'low')].values < piv_tab1[(1, 'high')].values)]
+        print(disp_tab1.to_dict())
+        piv_tab2 = ab_hr_data.pivot_table(index='time_spend_company', columns='promotion_last_5years',
+                                          values=['satisfaction_level', 'last_evaluation'], aggfunc=['min', 'max', 'mean'])
+        disp_tab2 = piv_tab2.loc[piv_tab2[('mean', 'last_evaluation', 0)] > piv_tab2[('mean', 'last_evaluation', 1)]]
+        print(disp_tab2.to_dict())
 
 
 if __name__ == '__main__':
