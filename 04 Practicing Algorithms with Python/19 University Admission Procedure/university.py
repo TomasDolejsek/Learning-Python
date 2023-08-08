faa10bb976c04cc80b06ab65066d8b9122f24cd2
TomasDolejsek <tdolejsek@centrum.cz>
2023-08-08 20:45:02 +0200

    Tomas Dolejsek
    Project 19 University Admission Procedure done!

--- 04 Practicing Algorithms with Python/19 University Admission Procedure/university.py
+++ 04 Practicing Algorithms with Python/19 University Admission Procedure/university.py
@@ -1,17 +1,21 @@
 class Applicant:
     def __init__(self, data):
         self.name = ' '. join(data[:2])
-        self.skills = [int(x) for x in data[2:6]]
-        self.priorities = data[6:]
+        self.skills = [float(x) for x in data[2:7]]
+        self.priorities = data[7:]
 
     def __str__(self):
         return f"{self.name} {self.skills} {self.priorities}"
 
-    def skills_mean(self, skill_ids):
-        summary = 0
-        for skill_id in skill_ids:
-            summary += self.skills[skill_id]
-        return summary / len(skill_ids)
+    def get_score(self, skill_names):
+        indexes = {'physics': 0, 'chemistry': 1, 'math': 2, 'computer science': 3}
+        result = 0
+        for name in skill_names:
+            result += self.skills[indexes[name]]
+        result /= len(skill_names)
+        if self.skills[4] > result:
+            result = self.skills[4]
+        return result
 
 
 class Department:
@@ -20,6 +24,13 @@ class Department:
         self.required_skills = skills
         self.accepted_students = []
 
+    def __str__(self):
+        text = list()
+        for student in sorted(self.accepted_students, key=lambda x: (-x.get_score(self.required_skills), x.name)):
+            text.append(f"{student.name} {student.get_score(self.required_skills)}")
+        text.append('')
+        return '\n'.join(text)
+
     @property
     def nstudents(self):
         return len(self.accepted_students)
@@ -27,47 +38,49 @@ class Department:
 
 class University:
     def __init__(self):
-        self.SKILL_INDEXES = {'physics': 0, 'chemistry': 1, 'math': 2, 'computer science': 3}
-        self.departments = []
-        self.create_departments()
+        self.departments = self.create_departments()
         self.start()
 
-    def create_departments(self):
-        self.departments.append(Department('Biotech', ['chemistry', 'physics']))
-        self.departments.append(Department('Chemistry', ['chemistry']))
-        self.departments.append(Department('Engineering', ['computer science', 'math']))
-        self.departments.append(Department('Mathematics', ['math']))
-        self.departments.append(Department('Physics', ['physics', 'math']))
+    @staticmethod
+    def create_departments():
+        departs = list()
+        departs.append(Department('Biotech', ['chemistry', 'physics']))
+        departs.append(Department('Chemistry', ['chemistry']))
+        departs.append(Department('Engineering', ['computer science', 'math']))
+        departs.append(Department('Mathematics', ['math']))
+        departs.append(Department('Physics', ['physics', 'math']))
+        return departs
 
-    def start(self):
+    def start(self, verbose=True):
         naccepted = int(input())
         applicants = []
-        with open('Data/applicants.txt') as file:
+        with open('Data/applicants.txt', 'r') as file:
             for line in file:
                 applicants.append(Applicant(line.split()))
-
-        for i in range(0, 2):
+        for i in range(0, 3):
             for depart in self.departments:
-                remaining = sorted(applicants, key=lambda x: (-float(x[depart.skill_index]), x[0]))
+                remaining = sorted(applicants, key=lambda x: (-x.get_score(depart.required_skills), x.name))
                 for applicant in remaining:
                     if depart.nstudents == naccepted:
                         break
-                    if applicant[i] == depart.name:
+                    if applicant.priorities[i] == depart.name:
                         depart.accepted_students.append(applicant)
                         applicants.remove(applicant)
+        if verbose:
+            self.display_department_students()
+        self.save_results()
 
-        self.display_departments()
+    def display_department_students(self):
+        for department in self.departments:
+            print(department.name)
+            print(department)
 
-    @staticmethod
-    def count_mean(skill_1, skill_2):
-        return (skill_1 + skill_2) / 2
-
-    def display_departments(self):
-        for depart in self.departments:
-            print(depart.name)
-            for student in sorted(depart.accepted_students, key=lambda x: (-float(x[2]), x[0])):
-                print(f"{student[0]} {student[1]} {student[2]}")
-            print()
+    def save_results(self):
+        for department in self.departments:
+            filename = department.name.lower()+'.txt'
+            with open(filename, 'w') as file:
+                file.write(department.__str__())
+        print("Results have been saved.")
 
 
 if __name__ == "__main__":
