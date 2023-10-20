460400d6df0e51867780bbc512bdbef86ec8b1fc
TomasDolejsek <tdolejsek@centrum.cz>
2023-10-20 10:47:02 +0200

    Tomas Dolejsek
    Project 07 Weather App done.

--- 06 Flask Developer/07 Weather App/app.py
+++ 06 Flask Developer/07 Weather App/app.py
@@ -7,7 +7,7 @@ import requests
 
 
 app = Flask(__name__)
-app.secret_key = 'abc'
+app.secret_key = "my very secret key"
 Base = declarative_base()
 
 
@@ -22,30 +22,35 @@ class DatabaseProcessor:
     def __init__(self):
         self.DATABASE_NAME = 'weather.db'
         engine = create_engine("sqlite:///" + self.DATABASE_NAME, echo=False)
-        Session = sessionmaker(bind=engine)
-        self.session = Session()
+        self.session = sessionmaker(bind=engine)()
         if not exists(self.DATABASE_NAME):
             Base.metadata.create_all(engine)
 
-    def get_city_names(self):
+    def get_cities(self):
         query = self.session.query(City)
-        return [x.name for x in query]
+        return [x for x in query]
 
-    def add_city(self, city_name):
-        cities = self.get_city_names()
-        if city_name.upper() in cities:
+    def add_city(self, city_id, city_name):
+        city_ids = [x.id for x in self.get_cities()]
+        if city_id in city_ids:
             return False
-        self.session.add(City(id=len(cities) + 1, name=city_name.upper()))
+        self.session.add(City(id=city_id, name=city_name))
         self.session.commit()
         return True
+
+    def delete_city(self, city_id):
+        query = self.session.query(City)
+        query.filter(City.id == city_id).delete()
+        self.session.commit()
 
 
 @app.route('/')
 def index():
     cities = []
-    for city in db.get_city_names():
+    for city in db.get_cities():
         city_dict = dict()
-        city_json = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=6e52c2b3e1a361f99484e92425c61b3f").json()
+        city_json = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city.name}&units=metric&appid=6e52c2b3e1a361f99484e92425c61b3f").json()
+        city_dict['id'] = city_json['id']
         city_dict['city_name'] = city_json['name'].upper()
         city_dict['current_temp'] = int(city_json['main']['temp'])
         city_dict['weather_state'] = city_json['weather'][0]['main']
@@ -55,12 +60,18 @@ def index():
 
 @app.route('/add', methods=['POST'])
 def add_city():
-    city = request.form['city_name']
-    check = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=6e52c2b3e1a361f99484e92425c61b3f").json()
-    if check['cod'] != 200:
+    city_name = request.form['city_name']
+    r_json = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=metric&appid=6e52c2b3e1a361f99484e92425c61b3f").json()
+    if r_json['cod'] != 200:
         flash("The city doesn't exist!")
-    elif not db.add_city(city):
+    elif not db.add_city(r_json['id'], city_name.upper()):
         flash("The city has already been added to the list!")
+    return redirect('/')
+
+
+@app.route('/delete/<city_id>', methods=['POST'])
+def delete_city(city_id):
+    db.delete_city(city_id)
     return redirect('/')
 
 
