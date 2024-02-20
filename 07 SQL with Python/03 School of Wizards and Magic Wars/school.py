82bbba5f8dfb0e9427554d377d192f6a0ba27cb4
TomasDolejsek <tdolejsek@centrum.cz>
2024-02-20 17:32:02 +0100

    Update school.py

--- 07 SQL with Python/03 School of Wizards and Magic Wars/school.py
+++ 07 SQL with Python/03 School of Wizards and Magic Wars/school.py
@@ -5,3 +5,12 @@ where grade = 3
 group by name
 having avg(result) = 5
 """
+
+achievement_point = """
+select name, sum(bonus) as 'bonus point' from students 
+join Student_Achievement on students.student_id = Student_Achievement.student_id
+join Achievement on Student_Achievement.achievement_id = Achievement.achievement_id
+group by name
+order by sum(bonus) desc
+limit 4
+"""
