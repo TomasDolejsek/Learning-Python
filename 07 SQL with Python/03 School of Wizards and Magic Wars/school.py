bd6a804ea2151ee3c9ab2eb61570f00cdfad8b46
TomasDolejsek <tdolejsek@centrum.cz>
2024-02-21 14:38:10 +0100

    Update school.py

--- 07 SQL with Python/03 School of Wizards and Magic Wars/school.py
+++ 07 SQL with Python/03 School of Wizards and Magic Wars/school.py
@@ -14,3 +14,22 @@ group by name
 order by sum(bonus) desc
 limit 4
 """
+
+average_student = """
+select name, CASE
+             when avg(result) > 3.5 then 'above average'
+             else 'below average'
+             end as best
+from Students
+join Student_Subject on Students.student_id = Student_Subject.student_id
+group by name
+"""
+
+best_of_department = """
+select name, department_name from Department
+join Students using (department_id)
+join Student_Subject using (student_id)
+group by name
+having avg(result) > 4.5
+order by name
+"""
